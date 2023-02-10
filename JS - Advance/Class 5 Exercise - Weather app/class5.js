2c6380d5f6a84a9a6881866afca22fd8b77f3d36
chrchevdj <chrchevdj@yahoo.com>
2023-02-10 21:02:23 +0200

    Homework

---
+++ JS - Advance/Class 5 Exercise - Weather app/class5.js
@@ -0,0 +1,27 @@
+/*
+1. Create an HTML page and connect a script, CSS document and libraries - Done
+2. Create the navigation bar, input, and button
+3. Connect the button with the script
+4. Create a function for calling the API by city name
+5. Connect the function with the input data when the button is clicked
+6. Build the navigation links/buttons mechanic to change the view
+7. Create a function that gets all the needed statistics and returns them
+Hint: use an object as a return value ( bundle the stats into one package )
+
+8. Create a function for printing the stats in the weather stats view
+9. Create a function for printing the table for hourly stats
+*/
+
+let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=bogdanci&units=metric&APPID=9fa272874ed1c73e31251bafeed2a69e"
+
+console.log(weatherUrl)
+
+// from code academy2 to everyone: 
+// 24dabb75e8592e9180e892e428dc3473
+// from code academy2 to everyone:  
+// 2095b65c75e8d13fe9e3b0e095b36936
+
+{/* <script
+src="https://code.jquery.com/jquery-3.6.3.min.js"
+integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+crossorigin="anonymous"></script> */}
